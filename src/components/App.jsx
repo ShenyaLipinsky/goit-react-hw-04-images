55c66aa1d1d40c03c6e8ac641db5116918362358
ShenyaLipinsky <shenyalipinskiy@gmail.com>
2022-07-16 13:43:09 +0300

    Whoops

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import Notiflix from 'notiflix';
 import { SearchBar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -6,11 +5,12 @@ import { Loader } from './Loader/Loader';
 import { Button } from './Button/Button';
 import { AppBox } from './App.styled';
 import { Modal } from './Modal/Modal';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import fetchImages from './services/API-Pixabay';
 
-const URI = 'https://pixabay.com/api/';
-const API_KEY = '27515696-8635174e5d1dc6e80848b95cf';
+const CARD_HEIGHT = 260;
 
-const axios = require('axios').default;
 const notiflixOptions = Notiflix.Notify.init({
   width: '400px',
   position: 'top-right',
@@ -21,144 +21,393 @@ const notiflixOptions = Notiflix.Notify.init({
   fontSize: '23px',
 });
 
-export class App extends Component {
-  state = {
-    page: 1,
-    queue: '',
-    hits: [],
-    loaderHidden: true,
-    totalHits: 0,
-    isLoading: false,
-    modalHidden: true,
-    modalImg: '',
-  };
+export const App = () => {
+  const [page, setPage] = useState(1);
+  const [queue, setQueue] = useState('');
+  const [hits, setHits] = useState([]);
+  const [loaderHidden, setLoaderHidden] = useState(true);
+  const [totalHits, setTotalHits] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [modalHidden, setModalHidden] = useState(true);
+  const [modalImg, setModalImg] = useState('');
 
-  getRequest = data => {
-    this.setState(prevState => {
+  const getRequest = data => {
+    setQueue(prevState => {
       if (
-        prevState.queue.toLowerCase().trim() ===
-          data.queue.toLowerCase().trim() &&
-        data.queue.toLowerCase().trim() !== ''
+        prevState.toLowerCase().trim() === data.toLowerCase().trim() &&
+        data.toLowerCase().trim() !== ''
       ) {
         return Notiflix.Notify.info(
-          `You are already looking for ${this.state.queue}. Change you request.`,
+          `You are already looking for ${data}. Change you request.`,
           notiflixOptions
         );
-      } else if (data.queue.toLowerCase() === '') {
+      } else if (data.toLowerCase() === '') {
+        setQueue('');
         return Notiflix.Notify.info(`Enter some request.`, notiflixOptions);
       } else {
-        return {
-          page: 1,
-          queue: data.queue,
-          hits: [],
-          totalHits: 0,
-        };
+        setPage(1);
+        setQueue(data);
+        setHits([]);
+        setTotalHits(0);
       }
     });
   };
 
-  timeout = () =>
-    setTimeout(() => {
-      window.scrollBy({
-        top: 1000,
-        behavior: 'smooth',
-      });
-    }, 400);
+  // const timeout = () =>
+  //   setTimeout(() => {
+  //     window.scrollBy({
+  //       top: CARD_HEIGHT * 2,
+  //       behavior: 'smooth',
+  //     });
+  //   }, 400);
 
-  loadMore = () => {
-    // this.timeout();
-    this.setState(prevState => ({ page: prevState.page + 1, isLoading: true }));
-  };
-  toggleModal = largeImg => {
-    if (this.state.modalImg === '') {
-      return this.setState({ modalHidden: false, modalImg: largeImg });
+  const toggleModal = largeImg => {
+    if (modalImg === '') {
+      setModalHidden(false);
+      return setModalImg(largeImg);
     }
-    return this.setState({ modalHidden: true, modalImg: '' });
+    setModalHidden(true);
+    return setModalImg('');
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.page !== this.state.page ||
-      prevState.queue !== this.state.queue
-    ) {
-      this.setState({ loaderHidden: false });
-      axios
-        .get(URI, {
-          params: {
-            q: this.state.queue,
-            page: this.state.page,
-            key: API_KEY,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            per_page: 12,
-          },
-        })
-        .then(response => {
-          if (response.data.totalHits !== 0 && this.state.totalHits === 0) {
-            Notiflix.Notify.success(
-              `Hooray! We found ${response.data.totalHits} images.`,
-              notiflixOptions
-            );
-          }
+  const loadMore = () => {
+    setIsLoading(true);
+    setPage(prevState => prevState + 1);
+  };
+  useEffect(() => {
+    if (queue === '') {
+      return;
+    }
 
-          if (this.state.hits.length < response.data.totalHits) {
-            let data = response.data.hits.map(item => {
-              let data = {
-                id: item.id,
-                webformatURL: item.webformatURL,
-                largeImageURL: item.largeImageURL,
-              };
-              return data;
-            });
-            this.setState(prevState => ({
-              totalHits: response.data.totalHits,
-              hits: [...prevState.hits, ...data],
-            }));
-          } else {
-            throw new Error('Oops');
-          }
-        })
-        .then(async () => {
-          if (this.state.hits.length > 0) {
-            this.timeout();
-          }
-          await clearTimeout(this.timeout);
-        })
-        .catch(error => {
-          console.log(error);
+    async function fetchImagesData() {
+      try {
+        setLoaderHidden(false);
+        const imagesData = await fetchImages(queue, page);
+        if (imagesData.totalHits === 0) {
           return Notiflix.Notify.failure(
-            'Oops something goes wrong, change your request or refresh page',
+            `Have no images on your request ${queue}`,
             notiflixOptions
           );
-        })
-        .then(() => {
-          this.setState({ loaderHidden: true, isLoading: false });
+        }
+        const images = imagesData.hits.map(item => {
+          let data = {
+            id: item.id,
+            webformatURL: item.webformatURL,
+            largeImageURL: item.largeImageURL,
+          };
+          return data;
         });
+        if (imagesData.totalHits !== 0 && totalHits === 0) {
+          setTotalHits(imagesData.totalHits);
+          Notiflix.Notify.success(
+            `Hooray! We found ${imagesData.totalHits} images.`,
+            notiflixOptions
+          );
+          return setHits(images);
+        } else {
+          return setHits(prevState => [...prevState, ...images]);
+        }
+      } catch {
+        return Notiflix.Notify.failure(
+          'Oops something goes wrong, change your request or refresh page',
+          notiflixOptions
+        );
+      } finally {
+        setIsLoading(false);
+        setLoaderHidden(true);
+      }
     }
-    return;
-  }
 
-  render() {
-    return (
-      <AppBox>
-        <SearchBar onSubmit={this.getRequest} />
-        {this.state.hits.length !== 0 && (
-          <ImageGallery
-            data={this.state.hits}
-            toggleModal={this.toggleModal}
-            isModalOpen={this.state.modalHidden}
-          />
-        )}
-        {!this.state.loaderHidden && <Loader />}
-        {this.state.hits.length < this.state.totalHits && (
-          <Button onClick={this.loadMore} loading={this.state.isLoading} />
-        )}
-        {!this.state.modalHidden && (
-          <Modal onClose={this.toggleModal}>
-            <img src={this.state.modalImg} alt="" />
-          </Modal>
-        )}
-      </AppBox>
-    );
-  }
-}
+    fetchImagesData();
+
+    return;
+    // axios
+    //   .get(URI, {
+    //     params: {
+    //       q: queue,
+    //       page: page,
+    //       key: API_KEY,
+    //       image_type: 'photo',
+    //       orientation: 'horizontal',
+    //       per_page: 12,
+    //     },
+    //   })
+    // .then(response => {
+    //   if (response.data.totalHits !== 0 && totalHits === 0) {
+    //     Notiflix.Notify.success(
+    //       `Hooray! We found ${response.data.totalHits} images.`,
+    //       notiflixOptions
+    //     );
+    //   }
+
+    //   if (hits.length < response.data.totalHits) {
+    //     let data = response.data.hits.map(item => {
+    //       let data = {
+    //         id: item.id,
+    //         webformatURL: item.webformatURL,
+    //         largeImageURL: item.largeImageURL,
+    //       };
+    //       return data;
+    //     });
+
+    //     return setHits(
+    //       prevState => [...prevState, ...data],
+    //     );
+    //   } else {
+    //     throw new Error('Oops');
+    //   }
+    // })
+    // .then(async () => {
+    //   if (hits.length > 0) {
+    //     timeout();
+    //   }
+    //   await clearTimeout(timeout);
+    // })
+    // .catch(error => {
+    //   console.log(error);
+    //   return Notiflix.Notify.failure(
+    //     'Oops something goes wrong, change your request or refresh page',
+    //     notiflixOptions
+    //   );
+    // })
+    // .then(() => {
+    //   setIsLoading(false);
+    //   setLoaderHidden(true);
+    // });
+  }, [page, queue, totalHits]);
+  useEffect(() => {
+    if (page > 1) {
+      window.scrollBy({
+        top: CARD_HEIGHT * 2,
+        behavior: 'smooth',
+      });
+    }
+  });
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (
+  //     prevState.page !== this.state.page ||
+  //     prevState.queue !== this.state.queue
+  //   ) {
+  //     this.setState({ loaderHidden: false });
+  //     axios
+  //       .get(URI, {
+  //         params: {
+  //           q: this.state.queue,
+  //           page: this.state.page,
+  //           key: API_KEY,
+  //           image_type: 'photo',
+  //           orientation: 'horizontal',
+  //           per_page: 12,
+  //         },
+  //       })
+  //       .then(response => {
+  //         if (response.data.totalHits !== 0 && this.state.totalHits === 0) {
+  //           Notiflix.Notify.success(
+  //             `Hooray! We found ${response.data.totalHits} images.`,
+  //             notiflixOptions
+  //           );
+  //         }
+
+  //         if (this.state.hits.length < response.data.totalHits) {
+  //           let data = response.data.hits.map(item => {
+  //             let data = {
+  //               id: item.id,
+  //               webformatURL: item.webformatURL,
+  //               largeImageURL: item.largeImageURL,
+  //             };
+  //             return data;
+  //           });
+  //           this.setState(prevState => ({
+  //             totalHits: response.data.totalHits,
+  //             hits: [...prevState.hits, ...data],
+  //           }));
+  //         } else {
+  //           throw new Error('Oops');
+  //         }
+  //       })
+  //       .then(async () => {
+  //         if (this.state.hits.length > 0) {
+  //           this.timeout();
+  //         }
+  //         await clearTimeout(this.timeout);
+  //       })
+  //       .catch(error => {
+  //         console.log(error);
+  //         return Notiflix.Notify.failure(
+  //           'Oops something goes wrong, change your request or refresh page',
+  //           notiflixOptions
+  //         );
+  //       })
+  //       .then(() => {
+  //         this.setState({ loaderHidden: true, isLoading: false });
+  //       });
+  //   }
+  //   // return;
+  // }
+
+  return (
+    <AppBox>
+      <SearchBar onSubmit={getRequest} />
+      {hits.length !== 0 && (
+        <ImageGallery
+          data={hits}
+          toggleModal={toggleModal}
+          isModalOpen={modalHidden}
+        />
+      )}
+      {!loaderHidden && <Loader />}
+      {hits.length < totalHits && (
+        <Button onClick={loadMore} loading={isLoading} />
+      )}
+      {!modalHidden && (
+        <Modal onClose={toggleModal}>
+          <img src={modalImg} alt="" />
+        </Modal>
+      )}
+    </AppBox>
+  );
+};
+
+// export class App extends Component {
+//   state = {
+//     page: 1,
+//     queue: '',
+//     hits: [],
+//     loaderHidden: true,
+//     totalHits: 0,
+//     isLoading: false,
+//     modalHidden: true,
+//     modalImg: '',
+//   };
+
+//   getRequest = data => {
+//     console.log(data);
+//     this.setState(prevState => {
+//       if (
+//         prevState.queue.toLowerCase().trim() ===
+//           data.queue.toLowerCase().trim() &&
+//         data.queue.toLowerCase().trim() !== ''
+//       ) {
+//         return Notiflix.Notify.info(
+//           `You are already looking for ${this.state.queue}. Change you request.`,
+//           notiflixOptions
+//         );
+//       } else if (data.queue.toLowerCase() === '') {
+//         return Notiflix.Notify.info(`Enter some request.`, notiflixOptions);
+//       } else {
+//         return {
+//           page: 1,
+//           queue: data.queue,
+//           hits: [],
+//           totalHits: 0,
+//         };
+//       }
+//     });
+//   };
+
+//   timeout = () =>
+//     setTimeout(() => {
+//       window.scrollBy({
+//         top: 1000,
+//         behavior: 'smooth',
+//       });
+//     }, 400);
+
+//   loadMore = () => {
+//     // this.timeout();
+//     this.setState(prevState => ({ page: prevState.page + 1, isLoading: true }));
+//   };
+//   toggleModal = largeImg => {
+//     if (this.state.modalImg === '') {
+//       return this.setState({ modalHidden: false, modalImg: largeImg });
+//     }
+//     return this.setState({ modalHidden: true, modalImg: '' });
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (
+//       prevState.page !== this.state.page ||
+//       prevState.queue !== this.state.queue
+//     ) {
+//       this.setState({ loaderHidden: false });
+//       axios
+//         .get(URI, {
+//           params: {
+//             q: this.state.queue,
+//             page: this.state.page,
+//             key: API_KEY,
+//             image_type: 'photo',
+//             orientation: 'horizontal',
+//             per_page: 12,
+//           },
+//         })
+//         .then(response => {
+//           if (response.data.totalHits !== 0 && this.state.totalHits === 0) {
+//             Notiflix.Notify.success(
+//               `Hooray! We found ${response.data.totalHits} images.`,
+//               notiflixOptions
+//             );
+//           }
+
+//           if (this.state.hits.length < response.data.totalHits) {
+//             let data = response.data.hits.map(item => {
+//               let data = {
+//                 id: item.id,
+//                 webformatURL: item.webformatURL,
+//                 largeImageURL: item.largeImageURL,
+//               };
+//               return data;
+//             });
+//             this.setState(prevState => ({
+//               totalHits: response.data.totalHits,
+//               hits: [...prevState.hits, ...data],
+//             }));
+//           } else {
+//             throw new Error('Oops');
+//           }
+//         })
+//         .then(async () => {
+//           if (this.state.hits.length > 0) {
+//             this.timeout();
+//           }
+//           await clearTimeout(this.timeout);
+//         })
+//         .catch(error => {
+//           console.log(error);
+//           return Notiflix.Notify.failure(
+//             'Oops something goes wrong, change your request or refresh page',
+//             notiflixOptions
+//           );
+//         })
+//         .then(() => {
+//           this.setState({ loaderHidden: true, isLoading: false });
+//         });
+//     }
+//     return;
+//   }
+
+//   render() {
+//     return (
+//       <AppBox>
+//         <SearchBar onSubmit={this.getRequest} />
+//         {this.state.hits.length !== 0 && (
+//           <ImageGallery
+//             data={this.state.hits}
+//             toggleModal={this.toggleModal}
+//             isModalOpen={this.state.modalHidden}
+//           />
+//         )}
+//         {!this.state.loaderHidden && <Loader />}
+//         {this.state.hits.length < this.state.totalHits && (
+//           <Button onClick={this.loadMore} loading={this.state.isLoading} />
+//         )}
+//         {!this.state.modalHidden && (
+//           <Modal onClose={this.toggleModal}>
+//             <img src={this.state.modalImg} alt="" />
+//           </Modal>
+//         )}
+//       </AppBox>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,5 @@
 import propTypes from 'prop-types';
-import { Component } from 'react';
+import { useState } from 'react';
 import {
   SearchBarBox,
   SearchBarForm,
@@ -8,51 +8,86 @@ import {
 } from './Searchbar.styled';
 import { BsSearch } from 'react-icons/bs';
 
-const INIT_VALUES = {
-  queue: '',
+export const SearchBar = ({ onSubmit }) => {
+  const [queue, setQueue] = useState('');
+
+  const handleChange = e => {
+    const { value } = e.target;
+    setQueue(value);
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(queue);
+  };
+  return (
+    <SearchBarBox>
+      <SearchBarForm onSubmit={handleSubmit}>
+        <SearchBarButton type="submit">
+          <BsSearch />
+        </SearchBarButton>
+        <SearchBarInput
+          type="text"
+          name="SearchBar"
+          autoComplete="off"
+          autoFocus
+          value={queue}
+          onChange={handleChange}
+          placeholder="Search images and photos"
+        />
+      </SearchBarForm>
+    </SearchBarBox>
+  );
 };
 
-export class SearchBar extends Component {
-  state = { ...INIT_VALUES };
+SearchBar.propTypes = {
+  onSubmit: propTypes.func.isRequired,
+};
 
-  static propTypes = {
-    onSubmit: propTypes.func.isRequired,
-  };
+// const INIT_VALUES = {
+//   queue: '',
+// };
 
-  handleChange = e => {
-    const { value } = e.target;
-    this.setState({ queue: value });
-  };
-  handleSubmit = e => {
-    const { onSubmit } = this.props;
-    e.preventDefault();
-    onSubmit({ ...this.state });
-    // this.reset();
-  };
+// export class SearchBar extends Component {
+//   state = { ...INIT_VALUES };
 
-  // reset = () => {
-  //   this.setState({ ...INIT_VALUES });
-  // };
+//   static propTypes = {
+//     onSubmit: propTypes.func.isRequired,
+//   };
 
-  render() {
-    const { queue } = this.state;
-    return (
-      <SearchBarBox>
-        <SearchBarForm onSubmit={this.handleSubmit}>
-          <SearchBarButton type="submit">
-            <BsSearch />
-          </SearchBarButton>
-          <SearchBarInput
-            type="text"
-            name="SearchBar"
-            autoComplete="off"
-            autoFocus
-            value={queue}
-            onChange={this.handleChange}
-            placeholder="Search images and photos"
-          />
-        </SearchBarForm>
-      </SearchBarBox>
-    );
-  }
-}
+//   handleChange = e => {
+//     const { value } = e.target;
+//     this.setState({ queue: value });
+//   };
+//   handleSubmit = e => {
+//     const { onSubmit } = this.props;
+//     e.preventDefault();
+//     onSubmit({ ...this.state });
+//     // this.reset();
+//   };
+
+//   // reset = () => {
+//   //   this.setState({ ...INIT_VALUES });
+//   // };
+
+//   render() {
+//     const { queue } = this.state;
+//     return (
+//       <SearchBarBox>
+//         <SearchBarForm onSubmit={this.handleSubmit}>
+//           <SearchBarButton type="submit">
+//             <BsSearch />
+//           </SearchBarButton>
+//           <SearchBarInput
+//             type="text"
+//             name="SearchBar"
+//             autoComplete="off"
+//             autoFocus
+//             value={queue}
+//             onChange={this.handleChange}
+//             placeholder="Search images and photos"
+//           />
+//         </SearchBarForm>
+//       </SearchBarBox>
+//     );
+//   }
+// }
